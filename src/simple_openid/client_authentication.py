fe765440986d5c515db0ed5f65feb564d3b759ad
Finn-Thorben Sell <dev@finn-thorben.me>
2022-11-05 17:15:57 +0100

    implement client authentication via basic auth

---
+++ src/simple_openid/client_authentication.py
@@ -0,0 +1,49 @@
+"""
+Utilities and data types used when authenticating the relying party (client).
+
+This should not be confused with authenticating a user as the user authentication at the OP is of no concern to us.
+Instead, these authenticate the relying party when e.g. exchanging tokens or requesting user information.
+It is a necessary process because while access tokens grant a relying party access to certain user information, the
+relying party must still identify itself to the OP when accessing that information.
+
+For more information visit `Section 9 of OpenID Connect Core 1.0 <https://openid.net/specs/openid-connect-core-1_0.html#ClientAuthentication>`_ and `Section 2.3 of OAuth 2.0 [RFC6749] <https://www.rfc-editor.org/rfc/rfc6749#section-2.3>`_.
+"""
+
+import abc
+
+from requests import models
+from requests.auth import AuthBase, HTTPBasicAuth
+
+# TODO Implement more client authentication methods
+
+
+class ClientAuthenticationMethod(AuthBase, metaclass=abc.ABCMeta):
+    """
+    A base class for client authentication methods which describes the interface that each method implements to authenticate requests.
+
+    This class also extends requests :class:`AuthBase` so that all derived implementations can directly be used with requests to authenticate something.
+    """
+
+    pass
+
+
+class NoneAuth(ClientAuthenticationMethod):
+    """
+    The Client does not authenticate itself at the Token Endpoint, either because it uses only the Implicit Flow (and so does not use the Token Endpoint) or because it is a Public Client with no Client Secret or other authentication mechanism.
+    """
+
+    def __call__(self, r: models.PreparedRequest) -> models.PreparedRequest:
+        return r
+
+
+class ClientSecretBasicAuth(ClientAuthenticationMethod, HTTPBasicAuth):
+    """
+    Clients that have received a `client_secret` value from the Authorization Server authenticate with the Authorization Server using the HTTP Basic authentication scheme.
+    """
+
+    def __init__(self, client_id: str, client_secret: str):
+        """
+        :param client_id: The client id which was set or issued during client registration
+        :param client_secret: The client secret which was issued during client registration
+        """
+        super().__init__(username=client_id, password=client_secret)
---
+++ tests/test_client_auth.py
@@ -0,0 +1,42 @@
+import copy
+from base64 import b64encode
+
+import pytest
+import requests
+from responses import RequestsMock
+
+from simple_openid.client_authentication import (
+    ClientSecretBasicAuth,
+    ClientSecretPostAuth,
+    NoneAuth,
+)
+
+
+@pytest.fixture
+def mock_empty_response(mocked_responses: RequestsMock):
+    mocked_responses.get(url="https://example.com")
+
+
+def test_none_auth(mock_empty_response):
+    # arrange
+    auth = NoneAuth()
+
+    # act
+    response = requests.get("https://example.com", auth=auth)
+
+    # assert
+    assert "Authorization" not in response.request.headers.keys()
+
+
+def test_client_secret_basic_auth(mock_empty_response):
+    # arrange
+    auth = ClientSecretBasicAuth("test-id", "foobar123")
+
+    # act
+    response = requests.get("https://example.com", auth=auth)
+
+    # assert
+    assert (
+        response.request.headers["Authorization"]
+        == f"Basic {b64encode(b':'.join(['test-id'.encode('ASCII'), 'foobar123'.encode('ASCII')])).decode('ASCII')}"
+    )
