ab3086e45481bfa654fab132adc5f65c81797f69
Finn-Thorben Sell <dev@finn-thorben.me>
2022-11-05 20:42:01 +0100

    test discovery error path

--- tests/conftest.py
+++ tests/conftest.py
@@ -157,6 +157,23 @@ def mock_known_provider_configs(mocked_responses: responses.RequestsMock):
     Mock requests to known OpenID provider config URLs to return static content
     """
     mocked_responses.get(
+        url="https://example.com/not-found/.well-known/openid-configuration",
+        content_type="text/plain",
+        body="Issuer config not found",
+        status=404,
+    )
+    mocked_responses.get(
+        url="https://example.com/invalid-json-document/.well-known/openid-configuration",
+        content_type="application/json",
+        body="{ 'hello': 'world' }",
+    )
+    mocked_responses.get(
+        url="https://example.com/invalid-json-syntax/.well-known/openid-configuration",
+        content_type="application/json",
+        body="{ 'hello }",
+    )
+
+    mocked_responses.get(
         url="https://identity.mafiasi.de/auth/realms/mafiasi/.well-known/openid-configuration",
         content_type="application/json",
         body='{"issuer":"https://identity.mafiasi.de/auth/realms/mafiasi","authorization_endpoint":"https://identity.mafiasi.de/auth/realms/mafiasi/protocol/openid-connect/auth","token_endpoint":"https://identity.mafiasi.de/auth/realms/mafiasi/protocol/openid-connect/token","introspection_endpoint":"https://identity.mafiasi.de/auth/realms/mafiasi/protocol/openid-connect/token/introspect","userinfo_endpoint":"https://identity.mafiasi.de/auth/realms/mafiasi/protocol/openid-connect/userinfo","end_session_endpoint":"https://identity.mafiasi.de/auth/realms/mafiasi/protocol/openid-connect/logout","jwks_uri":"https://identity.mafiasi.de/auth/realms/mafiasi/protocol/openid-connect/certs","check_session_iframe":"https://identity.mafiasi.de/auth/realms/mafiasi/protocol/openid-connect/login-status-iframe.html","grant_types_supported":["authorization_code","implicit","refresh_token","password","client_credentials"],"response_types_supported":["code","none","id_token","token","id_token token","code id_token","code token","code id_token token"],"subject_types_supported":["public","pairwise"],"id_token_signing_alg_values_supported":["PS384","ES384","RS384","HS256","HS512","ES256","RS256","HS384","ES512","PS256","PS512","RS512"],"id_token_encryption_alg_values_supported":["RSA-OAEP","RSA1_5"],"id_token_encryption_enc_values_supported":["A256GCM","A192GCM","A128GCM","A128CBC-HS256","A192CBC-HS384","A256CBC-HS512"],"userinfo_signing_alg_values_supported":["PS384","ES384","RS384","HS256","HS512","ES256","RS256","HS384","ES512","PS256","PS512","RS512","none"],"request_object_signing_alg_values_supported":["PS384","ES384","RS384","HS256","HS512","ES256","RS256","HS384","ES512","PS256","PS512","RS512","none"],"response_modes_supported":["query","fragment","form_post"],"registration_endpoint":"https://identity.mafiasi.de/auth/realms/mafiasi/clients-registrations/openid-connect","token_endpoint_auth_methods_supported":["private_key_jwt","client_secret_basic","client_secret_post","tls_client_auth","client_secret_jwt"],"token_endpoint_auth_signing_alg_values_supported":["PS384","ES384","RS384","HS256","HS512","ES256","RS256","HS384","ES512","PS256","PS512","RS512"],"claims_supported":["aud","sub","iss","auth_time","name","given_name","family_name","preferred_username","email","acr"],"claim_types_supported":["normal"],"claims_parameter_supported":false,"scopes_supported":["openid","offline_access","profile","email","address","phone","roles","web-origins","microprofile-jwt","shortlinks","dev-scope"],"request_parameter_supported":true,"request_uri_parameter_supported":true,"code_challenge_methods_supported":["plain","S256"],"tls_client_certificate_bound_access_tokens":true}',
--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -1,4 +1,7 @@
+import pytest
+
 from simple_openid import discovery
+from simple_openid.exceptions import OpenidProtocolError
 
 
 def test_mafiasi_identity(mock_known_provider_configs):
@@ -9,3 +12,16 @@ def test_mafiasi_identity(mock_known_provider_configs):
 
 def test_google(mock_known_provider_configs):
     discovery.discover_configuration_from_issuer("https://accounts.google.com/")
+
+
+def test_errors(mock_known_provider_configs):
+    with pytest.raises(OpenidProtocolError):
+        discovery.discover_configuration_from_issuer("https://example.com/not-found")
+    with pytest.raises(OpenidProtocolError):
+        discovery.discover_configuration_from_issuer(
+            "https://example.com/invalid-json-document"
+        )
+    with pytest.raises(OpenidProtocolError):
+        discovery.discover_configuration_from_issuer(
+            "https://example.com/invalid-json-syntax"
+        )
