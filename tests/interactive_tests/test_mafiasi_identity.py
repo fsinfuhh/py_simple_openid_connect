847bc24c364d98ce34645d5ce329dd91275d0d73
Finn-Thorben Sell <dev@finn-thorben.me>
2022-11-11 15:50:13 +0100

    add interactive test for confidential clients

--- tests/interactive_tests/test_mafiasi_identity.py
+++ tests/interactive_tests/test_mafiasi_identity.py
@@ -13,12 +13,57 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.interactive()
-def test_complete_login(real_app_server, secrets):
+def test_public_client(real_app_server, secrets):
     # arrange
     oidc_client = OpenidClient.from_issuer_url(
         "https://identity.mafiasi.de/auth/realms/simple_openid_test",
         real_app_server.login_callback_url,
         client_id=secrets["mafiasi_identity_public_client_id"],
+    )
+    token_response: TokenSuccessResponse
+
+    def on_login(_url: furl) -> Tuple[int, Mapping[str, str], str]:
+        url = oidc_client.authorization_code_flow.start_authentication()
+        return (
+            HTTPStatus.FOUND,
+            {
+                "Location": url,
+            },
+            f"Go to {url}",
+        )
+
+    def on_login_callback(url: furl) -> Tuple[int, Mapping[str, str], str]:
+        nonlocal token_response
+        response = oidc_client.authorization_code_flow.handle_authentication_result(
+            str(url)
+        )
+        assert isinstance(response, TokenSuccessResponse)
+        token_response = response
+        real_app_server.done()
+        return HTTPStatus.OK, {}, f"Success. You can close this tab."
+
+    # act (login)
+    logger.info(f"Visit {real_app_server.login_url}")
+    real_app_server.serve_until_done(on_login, on_login_callback)
+
+    # act (decode id token)
+    id_token = oidc_client.decode_id_token(token_response.id_token)
+    assert isinstance(id_token, IdToken)
+
+    # act (get userinfo)
+    response = oidc_client.fetch_userinfo(token_response.access_token)
+    assert isinstance(response, UserinfoSuccessResponse)
+    assert response.sub != ""
+
+
+@pytest.mark.interactive()
+def test_confidential_client(real_app_server, secrets):
+    # arrange
+    oidc_client = OpenidClient.from_issuer_url(
+        "https://identity.mafiasi.de/auth/realms/simple_openid_test",
+        real_app_server.login_callback_url,
+        client_id=secrets["mafiasi_identity_confidential_client_id"],
+        client_secret=secrets["mafiasi_identity_confidential_client_secret"],
     )
     token_response: TokenSuccessResponse
 
