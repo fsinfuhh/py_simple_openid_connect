c209753ed57439f89087516290b96db377fb8d6f
Finn-Thorben Sell <dev@finn-thorben.me>
2022-12-06 22:33:24 +0100

    fix drf integration docs not properly linking

--- src/simple_openid_connect/integrations/djangorestframework/__init__.py
+++ src/simple_openid_connect/integrations/djangorestframework/__init__.py
@@ -5,5 +5,5 @@ Django REST Framework integration for :mod:`simple_openid_connect`.
 # try to import drf spectacular extensions but silently ignore failures if drf-spectacular is not installed
 try:
     import simple_openid_connect.integrations.djangorestframework.drf_spectacular_schema
-except ImportError:
+except Exception:
     pass
