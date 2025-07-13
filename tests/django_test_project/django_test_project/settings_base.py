ba0308c5cd766c8c339fef22cec99d1900d00cef
Lilly Sell <sell@b1-systems.de>
2025-07-13 20:40:03 +0200

    add TokenVerificationMiddleware to django test project

--- tests/django_test_project/django_test_project/settings_base.py
+++ tests/django_test_project/django_test_project/settings_base.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "simple_openid_connect.integrations.django.middleware.TokenVerificationMiddleware",
 ]
 
 ROOT_URLCONF = "django_test_project.urls"
